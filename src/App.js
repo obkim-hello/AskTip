0dabe222f7686c0ee38566855b3accbd1a2dd69c
Ken Qijin Xu <ken.xu@wizrobotics.com>
2024-02-22 22:59:07 -0500

    change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className=" flex flex-col justify-center p-16 gap-6 h-full">
           {" "}
           <div>
-            <p className="text-4xl text-white">Add a Tip to KEN</p>
+            <p className="text-lg md:text-4xl text-white">Add a Tip to KEN</p>
           </div>
           <div className="grid grid-cols-2 w-full gap-6">
             <button
@@ -18,8 +18,8 @@ function App() {
                 alert("Thank you for your tip!");
               }}
             >
-              <p className="text-4xl">18%</p>
-              <p>Good</p>
+              <p className="text-lg md:text-4xl">18%</p>
+              <p className="text-xs md:text-lg">Good</p>
             </button>
             <button
               className=" rounded-lg border border-white text-white py-4 active:bg-slate-200 active:text-slate-800"
@@ -28,8 +28,8 @@ function App() {
                 alert("Thank you for your tip!");
               }}
             >
-              <p className="text-4xl">20%</p>
-              <p>Great</p>
+              <p className="text-lg md:text-4xl">20%</p>
+              <p className="text-xs md:text-lg">Great</p>
             </button>
             <button
               className=" rounded-lg border border-white text-white py-4 active:bg-slate-200 active:text-slate-800"
@@ -38,8 +38,8 @@ function App() {
                 alert("Thank you for your tip!");
               }}
             >
-              <p className="text-4xl">25%</p>
-              <p>Wow!</p>
+              <p className="text-lg md:text-4xl">25%</p>
+              <p className="text-xs md:text-lg">Wow!</p>
             </button>
             <button
               className=" rounded-lg border border-white text-white py-4 active:bg-slate-200 active:text-slate-800"
@@ -48,8 +48,8 @@ function App() {
                 alert("Thank you for your tip!");
               }}
             >
-              <p className="text-4xl">30%</p>
-              <p>Best Service Ever!</p>
+              <p className="text-lg md:text-4xl">30%</p>
+              <p className="text-xs md:text-lg">Best Service Ever!</p>
             </button>
             <button
               className=" rounded-lg border border-white text-white py-4 active:bg-slate-200 active:text-slate-800"
@@ -58,8 +58,8 @@ function App() {
                 alert("Thank you for your tip!");
               }}
             >
-              <p className="text-4xl">35%</p>
-              <p>Top Top TOP</p>
+              <p className="text-lg md:text-4xl">35%</p>
+              <p className="text-xs md:text-lg">Top Top Top</p>
             </button>
             <button
               className=" rounded-lg border border-white text-white py-4 active:bg-slate-200 active:text-slate-800"
@@ -69,7 +69,7 @@ function App() {
                 alert("Thank you for your tip of " + amount + "!");
               }}
             >
-              <p className="text-4xl">Custom</p>
+              <p className="text-lg md:text-4xl">Custom</p>
             </button>
           </div>
         </div>
